b156aa3d55e85de325440a3b44290b76ab38fba6
Hiru Wijemanne <hiruneshan@gmail.com>
2025-11-11 16:53:40 -0500

    Css change

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -8,24 +8,7 @@ import SocialLinks from "@/components/SocialLinks";
 import AboutMe from "@/components/AboutMe";
 
 export default function Home() {
-  // useEffect(() => {
-  //   const lenis = new Lenis({
-  //     duration: 1.2,
-  //     smooth: true,
-  //     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  //   });
 
-  //   function raf(time) {
-  //     lenis.raf(time);
-  //     requestAnimationFrame(raf);
-  //   }
-
-  //   requestAnimationFrame(raf);
-
-  //   return () => {
-  //     lenis.destroy();
-  //   };
-  // }, []);
 
   return (
     <>
