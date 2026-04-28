cbdad11770dcdce4f289348fbc022d10cf6bf5cf
Hiru Wijemanne <hiruneshan@gmail.com>
2026-04-27 20:15:02 -0400

    adding google verification

--- my-app/pages/_document.js
+++ my-app/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="04O044-MqksBc9FTmoNjyrKsqvgRAFIwU4o0VwGriME" />
+      </Head>
       <body>
         <Main />
         <NextScript />
